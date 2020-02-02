973fa62a87b2f5a3605780aa18384e57d83f0045
Gudzenko <o.gudzenko@wegree.com>
2020-02-02 17:03:56 +0100

    add filter

--- filter.py
+++ filter.py
@@ -1,9 +1,12 @@
 class Filter:
-    def __init__(self, size=3, coef=0.9):
+    def __init__(self, size=3, coef=0.9, t=1, dt=0.02):
         self.coef = coef
         self.size = size
+        self.T = t
+        self.dt = dt
         self.values = self.empty_array()
         self.data = self.empty_array()
+        self.prev_data = self.empty_array()
 
     def empty_array(self):
         values = []
@@ -13,8 +16,14 @@ class Filter:
 
     def add(self, data):
         self.data = data
+        K = self.dt / self.T / 2.0
+        K1 = self.dt / self.T
         for index in range(self.size):
-            self.values[index] = (1 - self.coef) * self.values[index] + self.coef * self.data[index]
+            # self.values[index] = (1 - self.coef) * self.values[index] + self.coef * self.data[index]
+            self.values[index] = self.values[index] * (1 - K) / (1 + K) + \
+                                 (self.data[index] + self.prev_data[index]) * K / (1 + K)
+            # self.values[index] = self.values[index] * (1 - K1) + K1 * self.data[index]
+            self.prev_data[index] = self.data[index]
 
     def get(self):
         return self.values
--- inertialSensors.py
+++ inertialSensors.py
@@ -3,19 +3,30 @@ import time
 import subprocess
 import math
 import os
+from filter import Filter
 
 
 class InertialSensorsThread(threading.Thread):
-    def __init__(self):
+    def __init__(self, save_to_file=False):
         threading.Thread.__init__(self)
         self.is_running = False
+        dt = 0.02
         self.is_new = True
         self.acceleration = []
+        self.filter_acceleration = Filter(size=3, dt=dt, t=0.04)
         self.orientation = []
+        self.filter_orientation = Filter(size=3, dt=dt, t=0.04)
         self.magnetometer = []
+        self.filter_magnetometer = Filter(size=3, dt=dt, t=0.04)
         self.gyro = []
+        self.filter_gyro = Filter(size=3, dt=dt, t=0.04)
         self.sensor_path = None
         self.time = 0.0
+        self.time_start = 0.0
+        self.save_to_file = save_to_file
+        if self.save_to_file:
+            self.file_name = "inertialSensorsLogs.csv"
+            self.file = open(self.file_name, "w")
 
     def run(self):
         print("Start inertial sensors thread")
@@ -24,9 +35,12 @@ class InertialSensorsThread(threading.Thread):
         self.orientation = [0, 0, 0]
         self.magnetometer = [0, 0, 0]
         self.gyro = [0, 0, 0]
-        self.time = time.time()
+        self.time_start = time.time()
+        self.time = self.time_start
         for line in self.convert_data(self.sensor_path):
             # print(line)
+            if not self.is_running:
+                break
             next_time = time.time()
             dt = next_time - self.time
             self.time = next_time
@@ -40,12 +54,66 @@ class InertialSensorsThread(threading.Thread):
             for index in range(3):
                 self.gyro[index] = (orientation[index] - self.orientation[index]) / dt * math.pi / 180.0
             self.is_new = True
+            self.filter_gyro.add(self.gyro)
             self.orientation = orientation
+            self.filter_orientation.add(self.orientation)
             self.acceleration = data_int[4:7]
+            self.filter_acceleration.add(self.acceleration)
             self.magnetometer = data_int[7:]
+            self.filter_magnetometer.add(self.magnetometer)
+            if self.save_to_file:
+                self.to_file()
+
+    def to_file(self):
+        data_to_file = ""
+
+        data_to_file += "Orientation;"
+        for item in list(self.orientation):
+            data_to_file += " {:8.3f};".format(item)
+
+        data_to_file += "OrientationFilter;"
+        for item in list(self.filter_orientation.get()):
+            data_to_file += " {:8.3f};".format(item)
+
+        data_to_file += "Acceleration;"
+        for item in list(self.acceleration):
+            data_to_file += " {:8.5f};".format(item)
+
+        data_to_file += "AccelerationFilter;"
+        for item in list(self.filter_acceleration.get()):
+            data_to_file += " {:8.5f};".format(item)
+
+        data_to_file += "Gyro;"
+        for item in list(self.gyro):
+            data_to_file += " {:8.5f};".format(item)
+
+        data_to_file += "GyroFilter;"
+        for item in list(self.filter_gyro.get()):
+            data_to_file += " {:8.5f};".format(item)
+
+        data_to_file += "Magnetometer;"
+        for item in list(self.magnetometer):
+            data_to_file += " {:8.3f};".format(item)
+
+        data_to_file += "MagnetometerFilter;"
+        for item in list(self.filter_magnetometer.get()):
+            data_to_file += " {:8.3f};".format(item)
+
+        self.file.write("Time;  {:12.6f}; ; {} ; \n".format(self.time - self.time_start, data_to_file))
 
     def stop(self):
         self.is_running = False
+        if self.save_to_file:
+            self.save_to_file = False
+            self.file.close()
+            self.file = open(self.file_name, "r")
+            data = self.file.read()
+            new_data = data.replace(".", ",")
+            self.file.close()
+            self.file = open(self.file_name, "w")
+            self.file.write(new_data)
+            self.file.close()
+        print('Stop')
 
     def __del__(self):
         self.stop()
@@ -92,13 +160,21 @@ class InertialSensorsThread(threading.Thread):
 
 
 if __name__ == "__main__":
-    app = InertialSensorsThread()
+    try:
+        os.system("sudo pigpiod -p 8888")
+        time.sleep(1)
+    except:
+        pass
+    app = InertialSensorsThread(save_to_file=True)
     app.config()
     app.start()
 
-    while True:
+    t = 0.0
+    while t < 10:
         time.sleep(0.01)
+        t += 0.01
         data = app.get_data()
         orient = (data[0])["orientation"]
         print("Data: time={:10.2f} x={:9.3f} y={:9.3f} z={:9.3f} is_new={}".format(data[2], orient[0], orient[1], orient[2], data[1]))
         # print("Data: {}".format((app.get_data())))
+    app.stop()
